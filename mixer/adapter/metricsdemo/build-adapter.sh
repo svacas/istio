74e97a42a625d49f815525d87f2141db49e370cc
svacas <svacas@gmail.com>
2019-09-24 13:49:20 -0300

    Build adapter in docker

--- mixer/adapter/metricsdemo/build-adapter.sh
+++ mixer/adapter/metricsdemo/build-adapter.sh
@@ -2,10 +2,18 @@
 
 set -e
 
-echo go generate...
+echo "go generate..."
 go generate ./...
 
-echo go build...
+echo "go build..."
 go build ./...
 
-echo done.
+echo "go build linux binary..."
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o bin/metricsd ./cmd/
+
+echo done
+
+## post
+
+# docker build -t 302195489440.dkr.ecr.us-east-1.amazonaws.com/mulesoft/metricsd-adapter:0.0.1_3 .
+# docker push 302195489440.dkr.ecr.us-east-1.amazonaws.com/mulesoft/metricsd-adapter:0.0.1_3
--- mixer/adapter/metricsdemo/run-client-mixer.sh
+++ mixer/adapter/metricsdemo/run-client-mixer.sh
@@ -7,5 +7,5 @@ pushd $ISTIO/istio && make mixc
 
 echo
 echo run mixer client...
-$GOPATH/out/darwin_amd64/release/mixc report -t request.time="2006-01-02T15:04:05Z",response.time="2006-01-02T15:04:05Z" -s destination.service.namespace="default",destination.service="svc.cluster.local",request.path="svc-path",request.method="get" -i request.size=1235
+$GOPATH/out/darwin_amd64/release/mixc report -t request.time="2006-01-02T15:04:05Z",response.time="2006-01-02T15:04:05Z" -s destination.service.namespace="default",destination.service.name="api_1",destination.service="svc.cluster.local",request.path="svc-path",request.method="get" -i request.size=1235
 
